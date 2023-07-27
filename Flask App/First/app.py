a7e191f6cafa328f295d65696c1bd11d7696bf74
RimpleDabas <125090606+RimpleDabas@users.noreply.github.com>
2023-07-27 12:15:01 -0400

    Basic code added

---
+++ Flask App/First/app.py
@@ -0,0 +1,60 @@
+from flask import Flask, render_template, request
+from tensorflow.keras.models import load_model
+from keras.preprocessing import image
+from tensorflow.keras.preprocessing.image import img_to_array
+import numpy as np 
+import cv2
+
+app = Flask(__name__)
+
+model = load_model('final-model-256.h5')
+
+def predict_label(image_path):
+	image = cv2.imread(image_path, cv2.IMREAD_COLOR)
+	image = cv2.resize(image, (256, 256))
+	image = img_to_array(image)
+	image = np.expand_dims(image,axis=0)
+	result =  model.predict(image)
+	array = result[0]
+	answer = np.argmax(array)
+	if  answer == 0:
+		print("Label: Bougainvillea")
+	elif answer == 1:
+		print("Label: Daffodil")
+	elif answer == 2:
+		print("Label: Dahlia")
+	elif answer == 3:
+		print("Label : Foxglove")
+	elif answer == 4: 
+		print("Label : Hibiscus")
+	elif answer == 5:
+		print("Label: Hydrangea")
+	elif answer == 6: 
+		print("Label : Orchid")
+	elif answer == 7:
+	    print("Label: Rose")
+	elif answer == 8 :
+		print("Label: Sunflower")    
+	elif answer == 9:
+	    print("Label: Tulip")
+	return answer    
+
+
+# routes
+@app.route("/", methods=['GET', 'POST'])
+def main():
+	return render_template("index.html")
+
+
+@app.route("/submit", methods = ['GET', 'POST'])
+def get_output():
+	if request.method == 'POST':
+		img = request.files['my_image']
+		image_path = "static/" + img.filename	
+		img.save(image_path)
+		p = predict_label(image_path)
+	return render_template("index.html", prediction = p, img_path = image_path)
+
+if __name__ =='__main__':
+	#app.debug = True
+	app.run(debug = True)
