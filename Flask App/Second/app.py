18f5f48431d54887874b3f00a6331934becc5fa9
RimpleDabas <125090606+RimpleDabas@users.noreply.github.com>
2023-08-01 11:33:04 -0400

    Basic flask code version 2 working

--- Flask App/Second/app.py
+++ Flask App/Second/app.py
@@ -2,17 +2,13 @@ from flask import Flask, render_template, request
 import numpy as np
 from keras.preprocessing.image import img_to_array
 from keras.models import load_model
-import cv2
 from keras.preprocessing import image
 
 app = Flask(__name__)
 
 # Loading the model
-model = load_model('model_74.h5')
+model = load_model('model_79.h5')
 flowers = {'0': 'Bougainvilliea', '1': 'Daffodil', '2': 'Dahlia', '3': 'Foxglove','4':'Hibiscus','5':'Hydrangea','6':'Orchid','7': 'Rose','8':'Sunflower','9': 'Tulip'}
-#flowers = ['Bougainvilliea', 'Daffodil', 'Dahlia',  'foxglove','Hibiscus','Hydrangea','Orchid', 'Rose','Sunflower', 'Tulip']
-
-Image_path = './uploads/Picture5.jpg'
 
 @app.route("/")
 def main():
@@ -22,18 +18,20 @@ def main():
 app = Flask(__name__)
 @app.route("/")
 def about():
-    return render_template('template.html', label='', imagesource=Image_path)
+    return render_template('index.html')
 
-@app.route('/', methods=['GET', 'POST'])
+@app.route('/submit', methods=['GET','POST'])
 def upload_file():
     if request.method == 'POST':
-        file = request.files['file'].save(Image_path)
+        file = request.files['my_image']
+        Image_path= "./static/" + file.filename	
+        file.save(Image_path)
         i = image.load_img(Image_path,target_size = (150,150))
         i = image.img_to_array(i)/255.0
         result = model.predict(np.expand_dims(i,0))
         answer = np.argmax(result,axis=1)[0]
         label = str(answer)
-        return render_template('template.html', label=flowers[label], imagesource= './uploads' + file.filename)
+    return render_template('index.html', prediction = flowers[label], img_path = Image_path)
 
 if __name__ == "__main__":
     app.run(debug=True)
