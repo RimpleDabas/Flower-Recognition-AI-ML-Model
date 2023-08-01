c55e066e60e3f5092aea09278d4ae44ffb041630
RimpleDabas <125090606+RimpleDabas@users.noreply.github.com>
2023-08-01 19:40:39 -0400

    Javascript for flask version 2 added

--- Flask App/Second/app.py
+++ Flask App/Second/app.py
@@ -2,12 +2,12 @@ from flask import Flask, render_template, request
 import numpy as np
 from keras.preprocessing.image import img_to_array
 from keras.models import load_model
-from keras.preprocessing import image
-
+#from keras.preprocessing import image
+import cv2
 app = Flask(__name__)
 
 # Loading the model
-model = load_model('model_79.h5')
+model = load_model('final-model-256.h5')
 flowers = {'0': 'Bougainvilliea', '1': 'Daffodil', '2': 'Dahlia', '3': 'Foxglove','4':'Hibiscus','5':'Hydrangea','6':'Orchid','7': 'Rose','8':'Sunflower','9': 'Tulip'}
 
 @app.route("/")
@@ -20,18 +20,21 @@ app = Flask(__name__)
 def about():
     return render_template('index.html')
 
-@app.route('/submit', methods=['GET','POST'])
+@app.route('/submit', methods=['GET','POST']) # submit route here is linked to html
 def upload_file():
     if request.method == 'POST':
         file = request.files['my_image']
         Image_path= "./static/" + file.filename	
         file.save(Image_path)
-        i = image.load_img(Image_path,target_size = (150,150))
-        i = image.img_to_array(i)/255.0
-        result = model.predict(np.expand_dims(i,0))
-        answer = np.argmax(result,axis=1)[0]
-        label = str(answer)
-    return render_template('index.html', prediction = flowers[label], img_path = Image_path)
+        #print(Image_path)
+        img = cv2.imread(Image_path, cv2.IMREAD_COLOR)
+        img = cv2.resize(img, (256, 256))
+        image = img_to_array(img)
+        image = np.expand_dims(image,axis=0)
+        #print(result)
+        label = str(np.argmax(model.predict(image),axis=1)[0])
+        print(label)
+    return render_template('index.html', prediction = flowers[label], img_path = Image_path) # prediction and img_path linked to html
 
 if __name__ == "__main__":
     app.run(debug=True)
